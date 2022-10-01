6c33579588f093d43e4ce87bbc006498cbbd2349
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-10-01 17:57:58 +0300

    app design

---
+++ SimpsonBook/DetailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsVC.swift
+//  SimpsonBook
+//
+//  Created by Ozan Çiçek on 1.10.2022.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TABLEviEW: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
