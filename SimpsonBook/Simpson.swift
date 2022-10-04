8822eba3bebc1e307817cf10a52e432a0e2a7238
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-10-04 18:31:33 +0300

    Simpson Class

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,25 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Ozan Çiçek on 4.10.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+    
+    let name : String
+    let job : String
+    let image : UIImage
+    
+    
+    init(name: String, job: String, image: UIImage) {
+        self.name = name
+        self.job = job
+        self.image = image
+    }
+    
+    
+}
--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -17,6 +17,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         TABLEviEW.delegate = self
         TABLEviEW.dataSource = self
+        
+        
+        let homer = Simpson(name: "homer", job: "Nuclear safety", image: UIImage(named: "Homer")!)
+        let Bart = Simpson(name: "Bart", job: "Student", image:UIImage(named: "Bart")!)
+        let Lisa = Simpson(name: "Lisa", job: "houseWife", image: UIImage(named: "Lisa")!)
+        let Maggie = Simpson(name: "Maggie", job: "baby", image: UIImage(named: "Maggie")!)
+        let Marge = Simpson(name: "Marge", job: "Student", image: UIImage(named: "Marge")!)
+        
+        let homeArray = [homer, Bart,Lisa,Marge,Maggie]
     }
     
     
